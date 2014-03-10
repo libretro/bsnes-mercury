d1f607e384b5590c11555bdf1cef576f8eb1d0d0
Alcaro <floating@muncher.se>
2014-03-11 00:55:36 +0100

    Fix build properly.

--- sfc/chip/dsp1/dsp1.cpp
+++ sfc/chip/dsp1/dsp1.cpp
@@ -4,8 +4,6 @@
 namespace SuperFamicom {
 
 DSP1 dsp1;
-DSP1DR dsp1dr;
-DSP1SR dsp1sr;
 
 #include "serialization.cpp"
 #include "dsp1emu.cpp"
@@ -13,21 +11,23 @@ DSP1SR dsp1sr;
 void DSP1::init() {
 }
 
-void DSP1::enable() {
+void DSP1::load() {
+}
+
+void DSP1::unload() {
 }
 
 void DSP1::power() {
-  reset();
 }
 
 void DSP1::reset() {
-  dsp1.reset();
 }
 
-uint8 DSP1DR::read(unsigned addr) { return dsp1.dsp1.getDr(); }
-void DSP1DR::write(unsigned addr, uint8 data) { dsp1.dsp1.setDr(data); }
+uint8 DSP1::read(unsigned addr) {
+}
 
-uint8 DSP1SR::read(unsigned addr) { return dsp1.dsp1.getSr(); }
-void DSP1SR::write(unsigned addr, uint8 data) {}
+void DSP1::write(unsigned addr, uint8 data) {
+  
+}
 
 }
