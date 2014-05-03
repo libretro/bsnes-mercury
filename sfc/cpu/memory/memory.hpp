34db224146b97fe5ab787e7f0f594be28ee16f1b
Alcaro <floating@muncher.se>
2014-05-03 23:40:24 +0200

    More inline! It doesn't do very much here, but a few things do happen.

--- sfc/cpu/memory/memory.hpp
+++ sfc/cpu/memory/memory.hpp
@@ -1,6 +1,6 @@
-void op_io();
-uint8 op_read(uint32 addr);
-void op_write(uint32 addr, uint8 data);
+inline void op_io();
+inline uint8 op_read(uint32 addr);
+inline void op_write(uint32 addr, uint8 data);
 alwaysinline unsigned speed(unsigned addr) const;
 
-uint8 disassembler_read(uint32 addr);
+inline uint8 disassembler_read(uint32 addr);
