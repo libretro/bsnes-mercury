0b28215cc7bf8b50eb5d29f29864f9c3cae4197e
Alcaro <floating@muncher.se>
2015-12-21 17:25:51 +0100

    Put back these diffs because I like them.

--- diff.sh
+++ diff.sh
@@ -1,6 +1,6 @@
 mv libco libretro-sdk
 cd ..
-diff -ru higan_v094-source bsneshle094 > bsneshle094/changes.diff
-cd bsneshle094
+diff -ru higan_v096-source bsneshle096 > bsneshle096/changes.diff
+cd bsneshle096
 mv libretro-sdk libco
 diff -u ../bsnes-libretro/target-libretro/libretro.cpp target-libretro/libretro.cpp > changes-libretro.diff
