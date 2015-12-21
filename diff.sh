77a198c35aaaefbdf57372c91b03ae86aa0e7762
Alcaro <floating@muncher.se>
2015-12-21 17:34:10 +0100

    Add more context here too.

--- diff.sh
+++ diff.sh
@@ -3,4 +3,4 @@ cd ..
 diff -rup higan_v096-source bsneshle096 > bsneshle096/changes.diff
 cd bsneshle096
 mv libretro-sdk libco
-diff -u ../bsnes-libretro/target-libretro/libretro.cpp target-libretro/libretro.cpp > changes-libretro.diff
+diff -up ../bsnes-libretro/target-libretro/libretro.cpp target-libretro/libretro.cpp > changes-libretro.diff
